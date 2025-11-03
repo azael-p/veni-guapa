390bb231974b5b4b9fdfe1716efcc1a4afaf2a71
azael-p <pignanessiazael@gmail.com>
2025-11-03 19:15:03 -0300

    actalizacion de privacidad

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ import { readFileSync } from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // ✅ Inicializar Firebase Admin con tu clave privada
 const serviceAccount = JSON.parse(
-  readFileSync("./serviceAccountKey.json", "utf8")
+  readFileSync(path.join(__dirname, "serviceAccountKey.json"), "utf8")
 );
 
 admin.initializeApp({
@@ -23,8 +26,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Carpeta pública principal (tienda)
 app.use(express.static(path.join(__dirname, "tienda")));
